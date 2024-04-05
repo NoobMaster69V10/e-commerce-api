b419f9bdbd51b1af1e0979ab271c5632fb1f1ca4
NoobMaster69V10 <giorgi.ubilava.1@btu.edu.ge>
2024-04-06 01:35:18 +0400

    Added jwt auth

--- ecommerceapi/ecommerceapi/urls.py
+++ ecommerceapi/ecommerceapi/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from mainapp.views import ProductViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = SimpleRouter()
 router.register(r'products', ProductViewSet)
 
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- ecommerceapi/mainapp/views.py
+++ ecommerceapi/mainapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import *
@@ -8,4 +10,5 @@ from .serializers import ProductSerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsAuthenticated,)
 
