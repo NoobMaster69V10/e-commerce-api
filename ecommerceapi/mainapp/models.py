2b845abde76796f260708f912e30f8fe8bc2b035
NoobMaster69V10 <giorgi.ubilava.1@btu.edu.ge>
2024-04-06 01:17:47 +0400

    Added main models

--- ecommerceapi/mainapp/models.py
+++ ecommerceapi/mainapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=0)
+    image = models.TextField(null=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
